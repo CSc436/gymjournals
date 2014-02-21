8a3479ff037e8bce312749b0cdc6a21ec76811ae
zuniga7 <zuniga7@email.arizona.edu>
2014-02-21 11:39:17 -0700

    starting using jquery/json to get information from the db

--- foundation/js/app.js
+++ foundation/js/app.js
@@ -1,1 +1,28 @@
 $(document).foundation();
+
+/*
+ *
+ *
+ */
+function getUser(){
+    $.ajax({
+    url: 'http://localhost:8000/api/users',
+    dataType: 'json',
+    success: function( data ) {
+        $.each(data, function( i, item ){
+            $("body").append( item +"\n" );
+        });
+    },
+    error: function( data ) {
+        console.log( data );
+        proccessError( data, 400, "Not able to get user information" );
+    }
+  });
+}
+
+function proccessError ( XHR, status, message ) {
+    //window.location.href = "error.html";
+    $("body").empty();
+    $("body").append(status +"\n");
+    $("body").append(message +"\n");
+}

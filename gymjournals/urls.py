2e4c05ddc97388a02011d49243aafc31f543fd83
Logan Chadderdon <lwchadderdon@gmail.com>
2014-02-21 11:46:37 -0700

    Modify settings and url to handle api.

--- gymjournals/urls.py
+++ gymjournals/urls.py
@@ -1,4 +1,5 @@
 from user import urls as userurls
+from api import urls as apiurls
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
@@ -11,4 +12,5 @@ urlpatterns = patterns(
         r"^api-auth/",
         include("rest_framework.urls", namespace="rest_framework")
     ),
+    url(r"^api/", include(apiurls)),
 )

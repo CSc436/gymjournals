f1b470a74cef2ac0e50b00ee427384ed86c614fb
storme-weather <stormeb@email.arizona.edu>
2014-02-22 12:58:12 -0700

    Made sure I would pass pep8 on my example test file.

--- gymjournals/tests/test_example.py
+++ gymjournals/tests/test_example.py
@@ -1,10 +1,10 @@
 def test_django():
     import settings as base_settings
     from django.conf import settings as django_settings
-    assert 3==5
+    assert 3 == 5
+
 
 def test_hurr():
     import settings as base_settings
     from django.conf import settings as django_settings
-    assert 5==5
-
+    assert 5 == 5

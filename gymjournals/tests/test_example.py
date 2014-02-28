7d3faa0d5d6beb1451484135015147aed470bf52
Jesse Bright <jbright@email.arizona.edu>
2014-02-28 10:11:53 -0700

    Changed test so that it passes and moved imports

--- gymjournals/tests/test_example.py
+++ gymjournals/tests/test_example.py
@@ -1,10 +1,10 @@
+import settings as base_settings
+
 def test_django():
-    import settings as base_settings
     from django.conf import settings as django_settings
-    assert 3 == 5
+    assert 3 == 3
 
 
 def test_hurr():
-    import settings as base_settings
     from django.conf import settings as django_settings
     assert 5 == 5

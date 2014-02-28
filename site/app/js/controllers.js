0b5def61a8ee3021547cd7e8b423075e5cbdc3ab
zuniga7 <zuniga7@email.arizona.edu>
2014-02-28 10:58:16 -0700

    preparing controllers on the html pages to do ajax

--- site/app/js/controllers.js
+++ site/app/js/controllers.js
@@ -1,10 +1,39 @@
 "use strict";
 
 /* Controllers */
+var server = "http://localhost:8000/"
 
 angular.module("myApp.controllers", []).
   controller("ExampleController", ["$scope", "$http", function($scope, $http) {
-    $http.get("http://localhost:8000/api/users/").success(
+    $http.get(server + "api/users/").success(
+      function(data, status, headers, config) {
+        $scope.data = data;
+      }
+    ).error(
+      function(data, status, headers, config) {
+        $scope.error = "There was an error.";
+      }
+    );
+  }]);
+
+var loginPage = angular.module('loginPage', []);
+
+
+loginPage.controller("loginCtrl", ["$scope", "$http", function($scope, $http) {
+    $http.get(server + "api/users/").success(
+      function(data, status, headers, config) {
+        $scope.data = data;
+        console.log($scope.data);
+      }
+    ).error(
+      function(data, status, headers, config) {
+        $scope.error = "There was an error.";
+      }
+    );
+  }]);
+
+loginPage.controller("registerCtrl", ["$scope", "$http", function($scope, $http) {
+    $http.get(server + "api/users/").success(
       function(data, status, headers, config) {
         $scope.data = data;
       }

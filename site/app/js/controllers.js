9d2ab8d07f246f8a1587d306ab0b322eebf57a7c
Logan Chadderdon <lwchadderdon@gmail.com>
2014-03-01 02:01:43 -0700

    Add test data to the `ExampleController`

--- site/app/js/controllers.js
+++ site/app/js/controllers.js
@@ -1,16 +1,20 @@
 "use strict";
 
-/* Controllers */
-
-angular.module("myApp.controllers", []).
-  controller("ExampleController", ["$scope", "$http", function($scope, $http) {
-    $http.get("http://localhost:8000/api/users/").success(
-      function(data, status, headers, config) {
-        $scope.data = data;
-      }
-    ).error(
-      function(data, status, headers, config) {
-        $scope.error = "There was an error.";
-      }
-    );
-  }]);
+angular.module("myApp.controllers", []).controller(
+  "ExampleController",
+  [
+    "$scope", "$http",
+    function($scope, $http) {
+      $http.get("http://localhost:8000/api/users/").success(
+        function(data, status, headers, config) {
+          $scope.data = data;
+        }
+      ).error(
+          function(data, status, headers, config) {
+            $scope.error = "There was an error.";
+          }
+      );
+      $scope.testData = "TEST DATA";
+    }
+  ]
+);

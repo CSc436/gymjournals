e7de91f9c4dfebdb6275773637e162c119f6bca5
storme-weather <stormeb@email.arizona.edu>
2014-03-29 12:26:38 -0700

    Deleted an extra pytest.ini file and altered the tests so they run in under a second

--- gymjournals/tests/db/test_user.py
+++ gymjournals/tests/db/test_user.py
@@ -12,7 +12,7 @@ def create_user(u_email, u_username, u_pwd):
     return SiteUser(email=u_email, username=u_username, pwd=u_pwd)
 
 
-@pytest.mark.django_db(transaction=True)
+@pytest.mark.django_db
 def test_default_state():
     '''
     This test case tests that the db is empty on start
@@ -21,7 +21,7 @@ def test_default_state():
     assert len(object_len) == 0
 
 
-@pytest.mark.django_db(transaction=True)
+@pytest.mark.django_db
 def test_user_information():
     '''
     This tests that a users information is
@@ -33,7 +33,7 @@ def test_user_information():
     assert jesse.pwd == 'lol'
 
 
-@pytest.mark.django_db(transaction=True)
+@pytest.mark.django_db
 def test_user_cant_save_no_info():
     '''
     Test that a SiteUser with not enough information
@@ -48,7 +48,7 @@ def test_user_cant_save_no_info():
     assert len(object_len) == 0
 
 
-@pytest.mark.django_db(transaction=True)
+@pytest.mark.django_db
 def test_user_cant_save_no_email():
     '''
     Test that a SiteUser with no email cannot
@@ -63,7 +63,7 @@ def test_user_cant_save_no_email():
     assert len(object_len) == 0
 
 
-@pytest.mark.django_db(transaction=True)
+@pytest.mark.django_db
 def test_user_cant_save_no_username():
     '''
     Test that a SiteUser with no username cannot
@@ -78,7 +78,7 @@ def test_user_cant_save_no_username():
     assert len(object_len) == 0
 
 
-@pytest.mark.django_db(transaction=True)
+@pytest.mark.django_db
 def test_user_cant_save_no_pwd():
     '''
     Test that a SiteUser with no pwd cannot
@@ -93,7 +93,7 @@ def test_user_cant_save_no_pwd():
     assert len(object_len) == 0
 
 
-@pytest.mark.django_db(transaction=True)
+@pytest.mark.django_db
 def test_user_cant_save_bad_email():
     '''
     Test that a SiteUser that enters an
@@ -108,7 +108,7 @@ def test_user_cant_save_bad_email():
     assert len(object_len) == 0
 
 
-@pytest.mark.django_db(transaction=True)
+@pytest.mark.django_db
 def test_user_cant_save_duplicate_email():
     '''
     Test that a SiteUser that enters a
@@ -122,11 +122,8 @@ def test_user_cant_save_duplicate_email():
     with pytest.raises(IntegrityError) as excinfo:
         duplicate.save()
 
-    object_len = SiteUser.objects.all()
-    assert len(object_len) == 1
 
-
-@pytest.mark.django_db(transaction=True)
+@pytest.mark.django_db
 def test_user_save():
     '''
     This tests that a user actually is created

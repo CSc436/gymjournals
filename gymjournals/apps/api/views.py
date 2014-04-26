2637bdb4f8f8336f7b85483e1d97fc24834894cd
Jesse Bright <jbright@email.arizona.edu>
2014-04-26 13:01:52 -0700

    Fixed API for exercises by tag

--- gymjournals/apps/api/views.py
+++ gymjournals/apps/api/views.py
@@ -165,7 +165,7 @@ class WeightGetAPIView(generics.RetrieveUpdateDestroyAPIView):
     model = Tag
 
 
-class ExerciseListAPIView(generics.ListCreateAPIView):
+class AerobicTagExerciseListAPIView(generics.ListCreateAPIView):
     model = Workout
 
     def get(self, request, format=None, **kwargs):
@@ -175,8 +175,16 @@ class ExerciseListAPIView(generics.ListCreateAPIView):
 
         to_return = []
 
+        tags = []
+        for t in Tag.objects.all():
+            if t.tag == tag:
+                tags.append(t.aerobic_exercise_id)
+
         for w in user.workout_set.order_by("date"):
-            exercises = list(w.aerobicexercise_set.filter(name=tag))
+            exercises = []
+            for ex in w.aerobicexercise_set.all():
+                if ex.id in tags:
+                    exercises.append(ex)
 
             if exercises:
                 exercises_json = []
@@ -195,11 +203,104 @@ class ExerciseListAPIView(generics.ListCreateAPIView):
         return Response(to_return)
 
 
+class WeightTagExerciseListAPIView(generics.ListCreateAPIView):
+    model = Workout
+
+    def get(self, request, format=None, **kwargs):
+        user_id = kwargs['user_id']
+        tag = kwargs['tag']
+        user = SiteUser.objects.filter(id=user_id).first()
+
+        to_return = []
+
+        tags = []
+        for t in Tag.objects.all():
+            if t.tag == tag:
+                tags.append(t.weight_exercise_id)
+
+        for w in user.workout_set.order_by("date"):
+            exercises = []
+            for ex in w.weightexercise_set.all():
+                if ex.id in tags:
+                    exercises.append(ex)
+
+            if exercises:
+                exercises_json = []
+                for exercise in exercises:
+                    h, m, s = [int(i) for i in
+                               str(exercise.duration).split(':')]
+                    exercise_millis = (3600 * h + 60 * m + s) * 1000
+                    millis = get_millis(w.date)
+                    e_json = {
+                        "duration": exercise_millis
+                    }
+                    exercises_json.append(e_json)
+                to_return.append([millis, exercises_json])
+
+        return Response(to_return)
+
+
 def get_millis(my_date):
     date = datetime.combine(my_date, datetime.min.time())
     seconds = (date - datetime.utcfromtimestamp(0)).total_seconds()
     millis = int(seconds * 1000)
     return millis
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+
+
+class TagUserListAPIView(generics.ListCreateAPIView):
+    serializer_class = TagSerializer
+    model = Tag
+
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']
+        return Tag.objects.filter(user=user_id).distinct()
+
+
+class TagWeightExerciseListAPIView(generics.ListCreateAPIView):
+    serializer_class = TagSerializer
+    model = Tag
+
+    def get_queryset(self):
+        exercise_id = self.kwargs['weight_id']
+        return Tag.objects.filter(weight_exercise=exercise_id).distinct()
+
+
+class TagAerobicExerciseListAPIView(generics.ListCreateAPIView):
+    serializer_class = TagSerializer
+    model = Tag
+
+    def get_queryset(self):
+        exercise_id = self.kwargs['aerobic_id']
+        return Tag.objects.filter(aerobic_exercise=exercise_id).distinct()
+
+
+class TagWorkoutListAPIView(generics.ListCreateAPIView):
+    serializer_class = TagSerializer
+    model = Tag
+
+    def get_queryset(self):
+        workout_id = self.kwargs['workout_id']
+        workout = Workout.objects.filter(id=workout_id).first()
+        weight_w = workout.weightexercise_set.all()
+        aerobic_w = workout.aerobicexercise_set.all()
+        weight_list = []
+        aerobic_list = []
+        for i in weight_w:
+            weight_list.append(i.id)
+        for i in aerobic_w:
+            weight_list.append(i.id)
+        to_return = []
+        for i in Tag.objects.all():
+            if i.weight_exercise_id in weight_list:
+                to_return.append(i)
+            elif i.aerobic_exercise_id in aerobic_list:
+                to_return.append(i)
+        return set(to_return)
 
 
 class TagSerializer(serializers.ModelSerializer):

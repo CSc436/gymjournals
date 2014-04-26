5edb23272d97f4924761cfc0185cf9475b7eda11
danbelcher <danbelcher@email.arizona.edu>
2014-04-26 12:58:29 -0700

    Changed directive.js to now resize the chart when the window is resized

--- site/app/js/directives.js
+++ site/app/js/directives.js
@@ -3,18 +3,31 @@
 /* Directives */
 
 var directives = angular.module('gymjournals.directives', []);
-
 directives.directive('weightChart', function() {
 
   return {
     restrict: "EA",
     scope: {},
     link: function(scope, element, attrs) {
-      // Beginning of Daniel's attempt at Javascript {{{
+
+
+      var svg = d3.select(element[0]).append("svg");
+
+      var data;
+      d3.json("http://localhost:8000/api/list/weights/1/", function(myData) {
+        myData.forEach(function(d) {
+          d.date = new Date(d[0]);
+          d.close = parseFloat(d[1]);
+        });
+        data = myData;
+        resize();
+      });
 
       d3.select(window).on('resize', resize);
 
       function resize() {
+          svg.selectAll('*').remove();
+      /*/ Beginning of Daniel's attempt at Javascript {{{ 
         // update width
         width = parseInt(d3.select('#weightChart').style('width'));
         width = width - margin.left - margin.right;
@@ -23,15 +36,18 @@ directives.directive('weightChart', function() {
         height = parseInt(d3.select('#weightChart').style('height'));
         height = height - margin.top - margin.bottom;
 
+        console.log("height: " + height);
+        console.log("width: " + width);
+
         // specify ranges for x and y axes
         x.range([0, width]);
         y.range([height, 0]);
-        console.log('did the thing!');
 
-      }
+        xAxis.scale(x);
+        yAxis.scale(y);
 
-
-      // end of Daniel's attempt }}}
+        area.y0(height);
+      // end of Daniel's attempt }}}*/
 
       var margin = {top: 20, right: 20, bottom: 30, left: 50},
           width = window.innerWidth - margin.left - margin.right,
@@ -56,18 +72,12 @@ directives.directive('weightChart', function() {
         .y0(height)
         .y1(function(d) { return y(d.close); });
 
-      var svg = d3.select(element[0]).append("svg")
-        .attr("id", "weightChart")
+      svg.attr("id", "weightChart")
         .attr("width", "100%")
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
+        .attr("id", "outerGroup")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-      d3.json("http://localhost:8000/api/list/weights/1/", function(data) {
-        data.forEach(function(d) {
-          d.date = new Date(d[0]);
-          d.close = parseFloat(d[1]);
-        });
 
         x.domain(d3.extent(data, function(d) { return d.date; }));
         y.domain([0, d3.max(data, function(d) { return d.close; })]);
@@ -91,6 +101,7 @@ directives.directive('weightChart', function() {
           .attr("dy", ".71em")
           .style("text-anchor", "end")
           .text("Price ($)");
-      });
+    }
+    
     }};
 });
